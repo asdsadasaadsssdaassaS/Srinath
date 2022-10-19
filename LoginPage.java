14b2ca510fa1b145c0f43df581061085a6b16bd5
srinath <jonnada.srinathmudiraj@gmail.com>
2022-10-19 14:10:01 +0530

    File

--- LoginPage.java
+++ LoginPage.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
+Srinath768
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
